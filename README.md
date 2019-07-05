7e0b16d7aa82e14ccaf061249f6cd5d502c07d37
Junior Abranches <juniorabranches@gmail.com>
2019-07-05 16:02:21 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cardjs-example
 An example of usage library Card.js on Ionic app
+
+## Telas / Screens: 
+
+![View this](src/assets/card1.png)
+
+# To run:
+* > npm install
+
+* > ionic serve (run in browser)
+
+or
+
+* > ionic cordova run android 
+
+or
+
+* > ionic cordova run ios 
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { CardModule } from 'ngx-card/ngx-card';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    CardModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
